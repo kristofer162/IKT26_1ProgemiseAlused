70dede552236f1fe17746e098992ba5748ea422a
kristofer162 <kito02218@gmail.com>
2026-09-10 10:57:16 +0300

    Ifandelsenumbers

---
+++ IfAndElseNumbers/IfAndElseNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace IfAndElseNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //konsoolis sisestame numbri
+            //see number salvestatakse string andmetüüpi
+            //toimub kontroll, kas sisetatud väärtus on number
+            //kasutada if ja else lauseid
+            string input = Console.ReadLine();
+
+            //kontrollime kas sistatud väärtus on number
+            if (int.TryParse(input, out int number))
+            {
+                Console.WriteLine("Sistatud number");
+            }
+            else
+            {
+                Console.WriteLine("Sisestaud väärtus ei ole number.");
+            }
+        }
+    }
+}
