c73fcf112ff7259a9133425541791962ee5cd711
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-17 10:44:02 +0300

    IfAndElseAge

---
+++ IfAndElseNesting/IfAndElseNesting/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace IfAndElseNesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //konsool küsib numbrit
+            //if võrdub 12
+            //else if ja siia sisse omakorda teha if and else. else if tingimus on, et
+            //muutuja on suurem, kui 20
+            //else ja seal väljastab konsool teksti: Mingid kahtlased väärtused
+
+            //konsool loeb ainult string andmetüüpe
+            string number = Console.ReadLine();
+            //muudame selle stringi int andmetüübiks ja ksautame Parset
+            int numberInt = int.Parse(number);
+
+            if (numberInt == 12)
+            {
+                Console.WriteLine("Number on 12");
+            }
+            else if (numberInt > 20)
+            {
+                if (numberInt > 30)
+                {
+                    Console.WriteLine("Sinu vanus n 31 või vanem");
+                }
+                else
+                {
+                    Console.WriteLine("Sinu vanus on 21 kuni 30");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Mingid kahtlased väärtused");
+            }
+        }
+    }
+}
