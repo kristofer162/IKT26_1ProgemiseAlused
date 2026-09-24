05fa35ee844e122710deedc66d65018df51ba9bc
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-24 10:24:55 +0300

    IfElseCar

---
+++ IfElseAutod/IfElseAutod/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace IfElseAutod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //kasutada if ja else
+            //kirjuta automark
+            //valikus on BMW, Audi, Porsche, Fiat ja Skoda
+            //Kui valitakse Skoda, siis seal sees on uuesti küsimus, et 
+            //mis mudelit soovid valida. Mudeli valikus on Kodiaq ja Octavia
+
+            Console.WriteLine("Sisesta autofirma");
+
+            string car = Console.ReadLine();
+
+            if(car == "Audi")
+            {
+                Console.WriteLine("Valisid Audi");
+            }
+            else if (car== "BMW")
+            {
+                Console.WriteLine("Valisid BMW");
+            }
+            else if (car == "Porsche")
+            {
+                Console.WriteLine("Valisid Porsche");
+            }
+            else if (car == "Fiat")
+            {
+                Console.WriteLine("Valisid Fiat");
+            }
+            else if (car == "Skoda")
+            {
+                Console.WriteLine("Valisid Skoda");
+                Console.WriteLine("Sisesta automudel");
+                String model = Console.ReadLine();
+                if (model == "Kodiaq")
+                {
+                    Console.WriteLine("Valisid Kodiaq");
+                }
+                else
+                {
+                    Console.WriteLine("Valisid Octavia");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ei valinud autot");
+            }
+        }
+    }
+}
